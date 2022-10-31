73a80cbb57229897fa8df76b4cb50234f1ce9bb4
Ainur Sabitov <ainursabitovak@mail.ru>
2022-10-31 09:02:14 +0300

    add model User

---
+++ src/main/java/ru/itis/kpfu/models/User.java
@@ -0,0 +1,16 @@
+package ru.itis.kpfu.models;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@ToString
+public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+}
